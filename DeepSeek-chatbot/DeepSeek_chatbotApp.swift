7eb20c844a1a81adc468c8d9c04dd46c1725502f
c <7160844@qq.com>
2025-05-23 12:54:57 +0800

    Initial Commit

---
+++ DeepSeek-chatbot/DeepSeek_chatbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepSeek_chatbotApp.swift
+//  DeepSeek-chatbot
+//
+//  Created by c on 2025/5/23.
+//
+
+import SwiftUI
+
+@main
+struct DeepSeek_chatbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
